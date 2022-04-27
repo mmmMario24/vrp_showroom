1375ce86e5bf21b1a63f3bfb17a7e4d74439c098
mmmMario <74585097+mmmMario24@users.noreply.github.com>
2022-04-27 19:51:43 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -5,6 +5,27 @@ rgb = {}
 spawnedVehs = {}
 
 curVehName = ""
+
+--- CallBacks 
+ServerCallbacks = {}
+CurrentRequestId = 0
+
+function TriggerServerCallback(name,cb,...)
+    ServerCallbacks[CurrentRequestId] = cb
+    TriggerServerEvent(GetCurrentResourceName()..'triggerServerCallback',name,CurrentRequestId,...)
+    if CurrentRequestId < 65535 then
+        CurrentRequestId = CurrentRequestId + 1
+    else
+        CurrentRequestId = 0
+    end
+end
+
+RegisterNetEvent(GetCurrentResourceName()..'serverCallback')
+AddEventHandler(GetCurrentResourceName()..'serverCallback', function(requestId,...)
+    ServerCallbacks[requestId](...)
+    ServerCallbacks[requestId] = nil
+end)
+
  
 Citizen.CreateThread(function()
     for k,va in pairs(Config.Vehicles) do 
